e9d35136e5cd6cb9019c0caee7efd8263847954d
qwnpng <qwnpng@gmail.com>
2016-12-30 17:13:14 +0000

    First pass: Count list recursively

--- easy_challenges.py
+++ easy_challenges.py
@@ -1,3 +1,19 @@
+def count_recursively(lst):
+    """ Count list using recursion
+
+    >>> count_recursively([])
+    0
+
+    >>> count_recursively([1, 2, 3])
+    3
+    """
+
+    if not lst:
+        return 0
+
+    return 1 + count_recursively(lst[1:])
+
+
 def binary_search(val):
     """ Guess for a val from 1 to 100
 

14787677f35592103bca6072b378bbe35b944d5a
qwnpng <qwnpng@gmail.com>
2017-01-05 02:34:02 +0000

    Started linked list problems with node class defined

---
+++ linked_list.py
@@ -0,0 +1,25 @@
+class Node(object):
+    """Class in a linked list."""
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def as_string(self):
+        """Represent data for this node and it's successors as a string.
+
+        >>> Node(3).as_string()
+        '3'
+
+        >>> Node(3, Node(2, Node(1))).as_string()
+        '321'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(out)

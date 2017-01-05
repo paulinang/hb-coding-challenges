81d82938d47ca75cd5e036d544698811a2b77675
qwnpng <qwnpng@gmail.com>
2017-01-05 02:40:05 +0000

    First pass: remove node from linked list

--- linked_list.py
+++ linked_list.py
@@ -23,3 +23,32 @@ class Node(object):
             n = n.next
 
         return "".join(out)
+
+
+def remove_node(node):
+    """Given a node in a linked list, remove it.
+
+    Remove this node from a linked list. Note that we do not have access to
+    any other nodes of the linked list, like the head or the tail.
+
+    Does not return anything; changes list in place.
+
+    >>> four_node = Node(4)
+    >>> three_node = Node(3, four_node)
+    >>> two_node = Node(2, three_node)
+    >>> one_node = Node(1, two_node)
+    >>> one_node.as_string()
+    '1234'
+    >>> remove_node(two_node)
+    >>> one_node.as_string()
+    '134'
+
+    """
+
+    node.data = node.next.data
+    node.next = node.next.next
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
